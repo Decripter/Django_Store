891a9d3de5c6f79090b7b9f538b708616626dbbf
Jeilson Araujo <jeilsonaraujo@icloud.com>
2022-01-29 17:01:25 -0300

    feat: adding loadding circle also to home page

--- django_store_vue/src/views/Home.vue
+++ django_store_vue/src/views/Home.vue
@@ -10,29 +10,32 @@
     <div class="columns is-multiline">
       <h2 class="is-size-2 has-text-centered">Latest Products</h2>
     </div>
-    <div class="columns is-multiline is-centered ">
-    <div
-      class="column is-3"
-      v-for="product in latestProducts"
-      v-bind:key="product.id">
+    <div class="columns is-multiline is-centered">
+      <div
+        class="column is-3"
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+      >
+        <div class="box">
+          <figure class="image mb-4">
+            <img :src="product.get_thumbnail" />
+          </figure>
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
 
-      <div class="box">
-        <figure class="image mb-4">
-          <img :src="product.get_thumbnail"/>
-        </figure>
-        <h3 class="is-size-4">{{ product.name }}</h3>
-        <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-
-        <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
+          <router-link
+            v-bind:to="product.get_absolute_url"
+            class="button is-dark mt-4"
+            >View details</router-link
+          >
+        </div>
       </div>
     </div>
-</div>
   </div>
 </template>
 
 <script>
-import axios from "axios"
-
+import axios from "axios";
 
 export default {
   name: "Home",
@@ -47,8 +50,10 @@ export default {
     this.getLatestProducts();
   },
   methods: {
-    getLatestProducts() {
-      axios
+    async getLatestProducts() {
+      this.$store.commit("setIsLoading", true);
+
+      await axios
         .get("/api/v1/latest-products/")
         .then((response) => {
           this.latestProducts = response.data;
@@ -56,6 +61,8 @@ export default {
         .catch((error) => {
           console.log(error);
         });
+
+      this.$store.commit("setIsLoading", false);
     },
   },
 };
